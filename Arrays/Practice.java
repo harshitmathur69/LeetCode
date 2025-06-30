ae9ea357cc57b0c071f0f3b0da7c3ccdf7472d58
Harshit <harshitmathur88@gmail.com>
2025-06-30 22:41:54 +0530

    Q744 new approach

--- Arrays/Practice.java
+++ Arrays/Practice.java
@@ -1,23 +1,12 @@
 package Arrays;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class Q744_FindSmallestLetterGreaterThanTarget {
-    public static void main(String[] args) {
+public class Practice{
+    public static void main(String[] args){
         char[] arr = {'c','f','j'};
-        char target = 'a';
+        System.out.println(nextGreatestLetter(arr,'d'));
     }
-//    public char nextGreatestLetter(char[] letters, char target){
-//        int start = 0;
-//        int end = letters.length - 1;
-//        while(start <= end){
-//            int mid = start + (end - start)/2;
-//            if(target < letters[mid]){
-//                end = mid - 1;
-//            }else{
-//                start = mid + 1;
-//            }
-//        }
-//        return letters[start % letters.length];
-//    }
     public static char nextGreatestLetter(char[] letters, char target){
         for(int i = 0; i < letters.length; i++){
             if(letters[i] > target){
